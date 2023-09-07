a9a17876a3c51b68a14431fa0b3d064c994b6207
2faceid <101102327+2faceid@users.noreply.github.com>
2023-09-07 19:53:28 +0300

    Update hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -6,4 +6,15 @@ def hello(name="World"):
 if __name__ == '__main__':
   fire.Fire(hello)
 
-  
+def calculate_vat(price, vat_rate):
+    vat_amount = price * (vat_rate / 100)
+    total_price = price + vat_amount
+    return vat_amount, total_price
+
+price = float(input("Enter the price: "))
+vat_rate = float(input("Enter the VAT rate (%): "))
+
+vat_amount, total_price = calculate_vat(price, vat_rate)
+
+print(f"VAT Amount: {vat_amount:.2f}")
+print(f"Total Price (including VAT): {total_price:.2f}")
