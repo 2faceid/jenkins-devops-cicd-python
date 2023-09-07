233607e011d6ced73b785c88e6f77620a9457bc0
2faceid <101102327+2faceid@users.noreply.github.com>
2023-09-07 20:04:35 +0300

    Update hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,18 +1,19 @@
 import fire
 
-# def hello(name="World"):
-#   return "Hello %s!" % name
+def hello(name="World"):
+  return "Hello %s!" % name
 
-# if __name__ == '__main__':
-#   fire.Fire(hello)
+if __name__ == '__main__':
+  fire.Fire(hello)
 
-def calculate_vat(price, vat_rate):
-    vat_amount = price * (vat_rate / 100)
-    total_price = price + vat_amount
+def calculate_vat(name="price",name="vat_rate"):
+    fire.Fire(vat_amount = fire.Fire(price * (vat_rate / 100)))
+    fire.Fire(total_price = price + vat_amount)
     return vat_amount, total_price
 
-price = float(input("Enter the price: "))
-vat_rate = float(input("Enter the VAT rate (%): "))
+fire.Fire(price = float(input("Enter the price: ")))
+fire.Fire(vat_rate = float(input("Enter the VAT rate (%): ")))
+
 
 vat_amount, total_price = calculate_vat(price, vat_rate)
 
